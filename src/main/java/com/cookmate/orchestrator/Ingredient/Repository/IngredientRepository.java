e8c8f92f1991db89c2338378fe255fe2f95f9db4
hyerin <rin4219@naver.com>
2025-12-14 20:32:08 +0900

    fix

--- src/main/java/com/cookmate/orchestrator/Ingredient/Repository/IngredientRepository.java
+++ src/main/java/com/cookmate/orchestrator/Ingredient/Repository/IngredientRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
-    Ingredient findByName(String name);
+    Ingredient findByKoreanName(String koreanName);
 }
